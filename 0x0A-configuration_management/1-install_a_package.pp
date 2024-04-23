c674f52bba7a28a83e081361d514600a51b7210b
davidddeveloper <davidconteh06@gmail.com>
2024-04-23 04:02:28 -0700

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,13 @@
+# Puppet manifest to install flask
+
+# Package resource to install pip
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Package resource to install flask using pip
+package { 'flask':
+  ensure   => '2.1.0', # Version of Flask
+  provider => 'pip', # Use pip to install flask
+  require  => Package['python3-pip'], # Ensure that pip is installed 
+}
