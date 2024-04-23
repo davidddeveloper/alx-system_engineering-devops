badeef99cb58772341ed8d63eabebd0bc76e2b37
davidddeveloper <davidconteh06@gmail.com>
2024-04-23 03:37:05 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,11 @@
-file { '/tmp/school'
-  ensure => '/tmp/school',
+# Puppet manifest to configure a web server
+
+# Define a file resource
+# to create a file (school) in /tmp/
+file { '/tmp/school':
+  ensure  => 'file',
   content => 'I love Puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data'
 }
