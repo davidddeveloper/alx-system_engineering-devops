6be4f14481c94bf8fc199a864d20e70c1ebbee82
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-27 11:17:40 -0700

    Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+$file_path='/etc/default/nginx'
+$content = "ULIMIT=\"-n 4096\""
+
+exec {'comment_out_ulimit':
+  command => 'sed -i "s/^ULIMIT=\"-n 15\"/# &/"'
+}
+
+exec {'the_sky_is_the_limit':
+  command => 'echo "$content" | sudo tee -a "$file_path"',
+}
