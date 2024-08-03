2d54821e618c1f7dc7492bcb3a654a4d76ca7331
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-02 18:29:58 -0700

    gets number of subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 """A python scripts that consume the reddit api
 to count the number of subscribers of a subreddit"""
-import praw
-
-
-client_id = '34s4jgg_lvfFbwwAF2-LmA'
-client_secret = 'F1nH_e_TAMQs5TjF_De0KcBvhEsQcQ'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-user_agent = f'{user_agent} (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-
-reddit = praw.Reddit(
-        client_id=client_id,
-        client_secret=client_secret,
-        user_agent=user_agent
-
-        )
+import requests
 
 
 def number_of_subscribers(subreddit):
     """ subscribers count """
+
+    endpoint = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
     try:
-        subreddit = reddit.subreddit(subreddit_name)
-        subscribers = subreddit.subscribers
+        response = requests.get(endpoint, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        subscribers = data['data']['subscribers']
         return subscribers
     except Exception:
         return 0
